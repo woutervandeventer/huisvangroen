725221ae651eb215dd310ac9a609a6c04e147492
Wouter van Deventer <wouter@Wouters-MacBook-Air.local>
2021-11-18 14:33:37 +0100

    Remove autoplay from video

--- components/About.js
+++ components/About.js
@@ -53,7 +53,7 @@ export default function About() {
           src="https://www.youtube.com/embed/ybhcISnMSN8"
           title="YouTube video player"
           frameBorder="0"
-          allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture"
+          allow="accelerometer; clipboard-write; encrypted-media; gyroscope; picture-in-picture"
           allowFullScreen
         ></iframe>
       </div>
